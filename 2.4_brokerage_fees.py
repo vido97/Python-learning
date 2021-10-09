3d6c28bba01fd7097e30a96f9af03c7afd3cbf29
Vi DO <89680015+vido97@users.noreply.github.com>
2021-10-09 16:27:52 +0300

    Update 2.4_brokerage_fees.py

--- 2.4_brokerage_fees.py
+++ 2.4_brokerage_fees.py
@@ -1,17 +1,21 @@
+#Write a program that asks the user for the values of the stock trades. 
+#The program calculates and prints the brokerage fees charged by the bank. 
+    #if the value of the the stock trade is less than 800 euros, the brokerage fee is 1 % of the transaction.
+    #if the value of the stock trade is at least 800 euros, the brokerage fee is 0.2 % of the transaction, or at least 8 euros.
+
 def main():
     print("Enter the values of the trades in separated lines. Stop with a negative value.")
-    line = input("Enter next value of the trade.\n")
-    value = float(line)
+    trade = float(input("Enter next value of the trade.\n"))
+    
     sum=0.0
-    while value >= 0:
-        if value >= 800:
-            fee=(value*0.002)
+    while trade >= 0:
+        if trade >= 800:
+            fee=(trade*0.002)
             if fee < 8:
                 fee=8
-        elif value <800:
-            fee =(value * 0.01)
-        line = input("Enter next value of the trade.\n")
-        value = float(line)
+        elif trade <800:
+            fee =(trade * 0.01)
+        trade = float(input(("Enter next value of the trade.\n"))
         sum = sum + fee
     print("The brokerage fees are",sum,"eur.")
 
