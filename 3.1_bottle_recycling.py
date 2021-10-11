fa6e5fac27890544b341b462df9a799fad9f6fa7
Vi DO <89680015+vido97@users.noreply.github.com>
2021-10-11 12:33:44 +0300

    Update 3.1_bottle_recycling.py

--- 3.1_bottle_recycling.py
+++ 3.1_bottle_recycling.py
@@ -24,28 +24,22 @@ def main():
     print("No deposit bottle: 5")
     bottle = int(input("Enter the type of the first bottle. Stop with a negative number:\n"))
 
-    
-    if bottle > 5:
-        print("Unknown bottle type.")
     total = 0
     while bottle >=0:
-        if bottle == 0:
-            total = total + 15
-        if bottle == 1:
-            total = total + 10
-        if bottle == 2:
-            total = total + 10
-        if bottle == 3:
-            total = total + 20
-        if bottle == 4:
-            total = total + 40
-        if bottle == 5:
-            total = total + 0
-        if bottle < 0:
-                break
+         if bottle == CAN:
+           total += CAN_DEPOSIT
+        elif bottle == GLASS_BOTTLE:
+           total += GLASS_BOTTLE_DEPOSIT
+        elif bottle == PLASTIC_SMALL:
+           total += PLASTIC_SMALL_DEPOSIT
+        elif bottle == PLASTIC_NORMAL:
+           total += PLASTIC_NORMAL_DEPOSIT
+        elif bottle == PLASTIC_BIG:
+           total += PLASTIC_BIG_DEPOSIT
+        elif bottle != NO_DEPOSIT_BOTTLE:
+           print("Unknown bottle type.")
         bottle = int(input("Enter the type of the next bottle. Stop with a negative number:\n"))
-        if bottle >5:
-            print("Unknown bottle type.")
+        
     print(f"You got {(total//100):d} euros and {(total%100):d} cents from the bottles.")
 
 main()
