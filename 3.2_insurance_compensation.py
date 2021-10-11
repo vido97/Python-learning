a2ec952353b52544a81fe3a705bef4445b1f6c50
Vi DO <89680015+vido97@users.noreply.github.com>
2021-10-11 12:40:04 +0300

    Update 3.2_insurance_compensation.py

--- 3.2_insurance_compensation.py
+++ 3.2_insurance_compensation.py
@@ -1,3 +1,9 @@
+AGE_REDUCTION_PERCENT = 0.25
+MAX_AGE_REDUCTION_PERCENT = 0.70
+MAX_COMPENSATION = 2500.0 # eur
+DEDUCTIBLE = 150.0 # eur
+CURRENT_YEAR = 2021
+
 def main():
 # 1. The program asks the user for the value of the device in euros until the value is greater than zero.
     deductable = 150
@@ -12,19 +18,19 @@ def main():
 
 # 3. If the value of the device is larger than deductible, ask for year:
     if price > deductable:
-        year= int (input("Enter the year of the purchase.\n"))
-        while year >2021:
-            year= int (input("The year is not valid. Enter again the year of the purchase.\n"))
+        purchase_year= int (input("Enter the year of the purchase.\n"))
+        while purchase_year > CURRENT_YEAR:
+            purchase_year= int (input("The year is not valid. Enter again the year of the purchase.\n"))
 
 #4. Calculate compensation
-        age_reduction = (2021-year)*0.25
-        if age_reduction > 0.7:
-            age_reduction = 0.7
+        age_reduction = (CURRENT_YEAR - purchase_year)*AGE_REDUCTION_PERCENT
+        if age_reduction > MAX_AGE_REDUCTION_PERCENT:
+            age_reduction = MAX_AGE_REDUCTION_PERCENT
         compensation = price - (deductable + (age_reduction*price))
 
-        if compensation >2500:
-            compensation = 2500
-        if compensation < 0:
+        if compensation > MAX_COMPENSATION:
+            compensation = MAX_COMPENSATION
+        elif compensation < 0:
             compensation = 0.0
     print("The compensation is",compensation, "eur.")
 
