f08ab0cb6e31fdcb51fef3d79d3a1364df5d8c1d
Vi DO <89680015+vido97@users.noreply.github.com>
2021-10-17 09:52:50 +0300

    Update 4.4_room_heating.py

--- 4.4_room_heating.py
+++ 4.4_room_heating.py
@@ -11,53 +11,40 @@ def calculate_warming(power, time, device_type, room_size):
     if (power<0 or time<0 or device_type not in ("computer", "lights", "oven", "washing machine")):
         return ERROR_WITH_WARMING
     elif device_type == "computer":
-        return (COMPUTER_HEATING_RATIO * power * (time*60)/(SPECIFIC_HEAT_CAPACITY_AIR * AIR_DENSITY * room_size))
+        heating_ratio = COMPUTER_HEATING_RATIO
     elif device_type == "lights":
-        return (LIGHTS_HEATING_RATIO * power * (time * 60) / (SPECIFIC_HEAT_CAPACITY_AIR * AIR_DENSITY * room_size))
+        heating_ratio = LIGHTS_HEATING_RATIOR
     elif device_type == "oven":
-        return (OVEN_HEATING_RATIO * power * (time * 60) / (SPECIFIC_HEAT_CAPACITY_AIR * AIR_DENSITY * room_size))
+        heating_ratio = OVEN_HEATING_RATIO
     elif device_type == "washing machine":
-        return (WASHING_MACHINE_HEATING_RATIO * power * (time * 60) / (SPECIFIC_HEAT_CAPACITY_AIR * AIR_DENSITY * room_size))
-
+        heating_ratio = WASHING_MACHINE_HEATING_RATIO
+    
+    tempurature = heating_ratio * power * (time * 60) / (SPECIFIC_HEAT_CAPACITY_AIR * AIR_DENSITY * room_size)
+    return tempurature
+    
 def calculate_costs(power, time):
     return KWH_PRICE * (power*0.001) * (time/60)
 
 def main():
     size=float(input("Enter the size of the room (m3).\n"))
-    device=str(input("Enter the device type (computer, lights, oven or washing machine).\n"))
-    power=float(input("Enter the power of the device (W).\n"))
-    time=int(input("Enter time of use (min).\n"))
-    heat = 0
-    cost = 0
+    new_input= 'yes'
+    heat_sum = 0
+    cost_sum = 0
 
-    if (power<0 or time<0 or device not in ("computer", "lights", "oven", "washing machine")):
-        print("Invalid input.")
-
-    else:
-        heat= calculate_warming(power, time, device,size)
-        cost= calculate_costs(power, time)
-
-    next_device=str(input("Do you want to enter another device (yes or no)?\n"))
-
-    while next_device == "yes":
+    while new_input = 'yes':
         device = str(input("Enter the device type (computer, lights, oven or washing machine).\n"))
         power = float(input("Enter the power of the device (W).\n"))
         time = int(input("Enter time of use (min).\n"))
-        if (power<0 or time<0 or device not in ("computer", "lights", "oven", "washing machine")):
+        heat = calculate_warming(power, time, device,size)
+        cost = calculate_costs(power, time)
+        
+        if heat == ERROR_WITH_WARMING:
             print("Invalid input.")
-
         else:
-            new_heat = calculate_warming(power, time, device,size)
-            new_cost = calculate_costs(power, time)
-            heat= heat + new_heat
-            cost= cost + new_cost
-
-        next_device = str(input("Do you want to enter another device (yes or no)?\n"))
+            heat_sum += heat
+            cost_sum += cost
+  
+        new_input=str(input("Do you want to enter another device (yes or no)?\n"))
 
     print(f"The electric devices heat the room by {heat:.2f} degrees and it costs {cost:.2f} cents.")
-
-
-
-
-
 main()
